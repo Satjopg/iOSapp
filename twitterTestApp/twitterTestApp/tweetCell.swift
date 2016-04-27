629c0bd4fd69a915d8c9393e551c04679dc099f4
Satoru Murakami <Satoru@SatsMac.local>
2016-04-27 12:39:04 +0900

    add new sample program

---
+++ twitterTestApp/twitterTestApp/tweetCell.swift
@@ -0,0 +1,24 @@
+//
+//  tweetCell.swift
+//  twitterTestApp
+//
+//  Created by Satoru Murakami on 2016/03/28.
+//  Copyright © 2016年 Satoru Murakami. All rights reserved.
+//
+
+import UIKit
+
+class tweetCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
