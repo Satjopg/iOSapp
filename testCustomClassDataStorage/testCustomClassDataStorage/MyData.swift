f7548eecdbe5c3796c20e2afd5a99175ea1b82df
Satoru Murakami <Satoru@SatsMac.local>
2016-05-02 18:29:50 +0900

    add new program

---
+++ testCustomClassDataStorage/testCustomClassDataStorage/MyData.swift
@@ -0,0 +1,9 @@
+//
+//  MyData.swift
+//  testCustomClassDataStorage
+//
+//  Created by Satoru Murakami on 2016/05/02.
+//  Copyright © 2016年 Satoru Murakami. All rights reserved.
+//
+
+import Foundation
