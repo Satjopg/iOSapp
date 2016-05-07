69e219ba40afb054de8c96b8d0e2b9d24966f6a2
Satoru Murakami <Satoru@SatsMac.local>
2016-05-08 01:29:26 +0900

    add app's function

--- MyToDoList/MyToDoList/ViewController.swift
+++ MyToDoList/MyToDoList/ViewController.swift
@@ -7,13 +7,13 @@
 //
 
 /*
-    永続的なデータの扱いの練習としてtodoリストアプリの作成を行う
-    このクラスの役割は
-    ・テーブルが操作された時のアクション
-    ・テーブルの表示に利用するデータ管理
-    である。
-    これはそれぞれdelegateとdatasourceで定義されているので
-    これを継承して、このクラスで処理をすることを明示する。
+永続的なデータの扱いの練習としてtodoリストアプリの作成を行う
+このクラスの役割は
+・テーブルが操作された時のアクション
+・テーブルの表示に利用するデータ管理
+である。
+これはそれぞれdelegateとdatasourceで定義されているので
+これを継承して、このクラスで処理をすることを明示する。
 */
 
 import UIKit
@@ -23,16 +23,34 @@ class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSourc
     // Tableviewのインスタンス
     @IBOutlet weak var tableview: UITableView!
     // todolistのデータを保持する配列
-    var todoList = [String]()
+    var todoList = [MyToDo]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 保存されたデータを読み込む
+        // NSData→MyToDoにデシリアライズ処理(unarchiver)を行う
+        let userDefault = NSUserDefaults.standardUserDefaults()
+        if let todoListData = userDefault.objectForKey("todoList") as? NSData {
+            if let storedTodoList = NSKeyedUnarchiver.unarchiveObjectWithData(todoListData) as? [MyToDo] {
+                todoList.appendContentsOf(storedTodoList)
+            }
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
     
     // +ボタンをおした時の処理
     @IBAction func barButton(sender: UIBarButtonItem) {
         
         // アラートダイアログ生成
         let alertController = UIAlertController(
-        title: "TODO追加",
-        message: "TODOを入力してください",
-        preferredStyle: UIAlertControllerStyle.Alert)
+            title: "TODO追加",
+            message: "TODOを入力してください",
+            preferredStyle: UIAlertControllerStyle.Alert)
         
         // テキストエリア追加
         alertController.addTextFieldWithConfigurationHandler(nil)
@@ -42,18 +60,26 @@ class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSourc
             title: "OK",
             style: UIAlertActionStyle.Default)
             { (action:UIAlertAction) -> Void in
-            // ボタンが押された時の処理
-            // テーブルビューの先頭にセルを加える
-            
-            // 初期値nilなのでアンラップ処理
-            if let textfield = alertController.textFields?.first {
-                // 先頭に追加する - 配列のメソッドinsertの利用
-                self.todoList.insert(textfield.text!, atIndex: 0)
+                // ボタンが押された時の処理
+                // テーブルビューの先頭にセルを加える
                 
-                // テーブルに行（セル）が追加されたことを通知
-                // 通知を行うことで再描画されて実際に見える形で追加される
-                self.tableview.insertRowsAtIndexPaths([NSIndexPath(forRow: 0, inSection: 0)], withRowAnimation: UITableViewRowAnimation.Right)
-            }
+                // 初期値nilなのでアンラップ処理
+                if let textfield = alertController.textFields?.first {
+                    let todo = MyToDo()
+                    todo.todoTitle = textfield.text!
+                    // 先頭に追加する - 配列のメソッドinsertの利用
+                    self.todoList.insert(todo, atIndex: 0)
+                    // テーブルに行（セル）が追加されたことを通知
+                    // 通知を行うことで再描画されて実際に見える形で追加される
+                    self.tableview.insertRowsAtIndexPaths([NSIndexPath(forRow: 0, inSection: 0)], withRowAnimation: UITableViewRowAnimation.Right)
+                }
+                // MyToDoをNSDataにシリアライズ(archive)する
+                let data:NSData = NSKeyedArchiver.archivedDataWithRootObject(self.todoList)
+                
+                // データを永続的に利用するために保存する
+                let userDefaults = NSUserDefaults.standardUserDefaults()
+                userDefaults.setObject(data, forKey: "todoList")
+                userDefaults.synchronize()
         }
         //　アラートダイアログにボタンを生成
         alertController.addAction(okaction)
@@ -69,28 +95,83 @@ class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSourc
         // ダイアログの可視化
         presentViewController(alertController, animated: true, completion: nil)
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-
+    
+    
     // テーブルの行数（セルの数）を返す
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
         // データの数だけセルはあるのでそれを返す
         return self.todoList.count
     }
+    
     // 表示するセルを返す
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
         // storyboardで指定したtodoCellを利用して再利用可能なセルを取得する
         let cell = tableview.dequeueReusableCellWithIdentifier("todoCell", forIndexPath: indexPath)
         // 行番号にあったものを取得
-        let todoTitle = todoList[indexPath.row]
+        let todo = self.todoList[indexPath.row]
         // cellのラベルに追加した内容を描画
-        cell.textLabel!.text = todoTitle
+        cell.textLabel!.text = todo.todoTitle
+        if todo.todoDone {
+            // 完了済みの時はチェックマークをつける
+            cell.accessoryType = UITableViewCellAccessoryType.Checkmark
+        } else {
+            cell.accessoryType = UITableViewCellAccessoryType.None
+        }
         return cell
     }
+    
+    // セルをタップした時の処理
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // タップしたセルのデータ取得
+        let todo = todoList[indexPath.row]
+        if todo.todoDone {
+            // 完了済みの場合未完に変更
+            todo.todoDone = false
+        } else {
+            // 未完の場合は完了済みに変更
+            todo.todoDone = true
+        }
+        // セルの状態を変更（再読み込み）
+        // withRowAnimation: UITableViewRowAnimation.Fade: アニメーションを発生）
+        tableview.reloadRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Fade)
+        
+        // MyToDoをNSDataにシリアライズする
+        let data:NSData = NSKeyedArchiver.archivedDataWithRootObject(self.todoList)
+        
+        // データを永続的に利用するために保存する
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        userDefaults.setObject(data, forKey: "todoList")
+        userDefaults.synchronize()
+    }
+    
+    
+    /**
+     MyToDo
+     - parameters:
+     - todoTitle1: 「やること」の内容
+     - todoDone: やったかどうかの判定
+     */
+    
+    class MyToDo:NSObject, NSCoding {
+        var todoTitle: String?
+        
+        var todoDone: Bool = false
+        // コンストラクタ
+        override init(){
+            
+        }
+        // デシリアライズ（デコード）処理（NSCodingプロトコルから継承）
+        required init?(coder aDecoder: NSCoder) {
+            todoTitle = aDecoder.decodeObjectForKey("todoTitle") as? String
+            todoDone = aDecoder.decodeBoolForKey("todoDone")
+        }
+        
+        // シリアライズ（エンコード）処理（NSCodingプロトコルから継承）
+        func encodeWithCoder(aCoder: NSCoder) {
+            aCoder.encodeObject(todoTitle, forKey: "todoTitle")
+            aCoder.encodeBool(todoDone, forKey: "todoDone")
+        }
+    }
+    
 }
 
