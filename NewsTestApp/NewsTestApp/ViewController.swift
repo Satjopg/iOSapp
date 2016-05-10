368d96d936d4ebe98fcdb5243ba20e48e03cf0d3
Satoru Murakami <Satoru@SatsMac.local>
2016-05-10 14:23:15 +0900

    add New Project.

---
+++ NewsTestApp/NewsTestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsTestApp
+//
+//  Created by Satoru Murakami on 2016/05/10.
+//  Copyright © 2016年 Satoru Murakami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
