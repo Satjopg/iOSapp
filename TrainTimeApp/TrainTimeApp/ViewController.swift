2fa4b5444511a811402a9edbd2792170c273bdc2
Satjopg <sss.jopg@me.com>
2016-05-20 02:14:08 +0900

    add code and add UI in storyboard

--- TrainTimeApp/TrainTimeApp/ViewController.swift
+++ TrainTimeApp/TrainTimeApp/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//  出発駅の入力欄
+    @IBOutlet weak var startField: UITextField!
+    
+//  到着駅の入力欄
+    @IBOutlet weak var endField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+//  検索ボタンを押した時の処理
+    @IBAction func searchButton(sender: AnyObject) {
+        if let start = startField.text {
+            print(start)
+        }
+        if let end = endField.text {
+            print(end)
+        }
+    }
 }
 
