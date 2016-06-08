3218ba30f6f8b2f5d01dbb671725e6b140669c4a
Satjopg <sss.jopg@me.com>
2016-06-08 17:03:11 +0900

    add refresh

--- QiitaTestApp/QiitaTestApp/QiitaData.swift
+++ QiitaTestApp/QiitaTestApp/QiitaData.swift
@@ -16,7 +16,7 @@ func getArticles() -> [[String: String?]] {
     var keep:Bool = true
     var articles: [[String: String?]] = []
 //    以下、記事取得
-        Alamofire.request(.GET, "https://qiita.com/api/v2/items")
+        Alamofire.request(.GET, "https://qiita.com/api/v2/items?per_page=40")
             .responseJSON { response in
                 guard let object = response.result.value else {
                     return
@@ -25,7 +25,8 @@ func getArticles() -> [[String: String?]] {
                 json_data.forEach{ (_, json_data) in
                     let article: [String:String?] = [
                         "title":json_data["title"].string,
-                        "userID":json_data["user"]["id"].string
+                        "userID":json_data["user"]["id"].string,
+                        "url":json_data["url"].string
                     ]
                     articles.append(article)
                 }
@@ -37,4 +38,59 @@ func getArticles() -> [[String: String?]] {
 //        response処理待ち
     }
     return articles
+}
+
+func getTags() -> [String]{
+//  タグ一覧を保持する
+    var tags:[String] = []
+//  待機か、実行状態をコントロールする変数
+    var keep:Bool = true
+//  以下、タグ一覧取得
+    Alamofire.request(.GET, "https://qiita.com/api/v2/tags?sort=count&per_page=40")
+    .responseJSON { response in
+        guard let object = response.result.value else {
+            return
+        }
+        let json_data = JSON(object)
+        json_data.forEach{ (_, json_data) in
+            let tag:String = json_data["id"].string!
+            tags.append(tag)
+        }
+        keep = false
+    }
+    
+    let runloop = NSRunLoop.currentRunLoop()
+    while keep && runloop.runMode(NSDefaultRunLoopMode, beforeDate: NSDate(timeIntervalSinceNow: 0.1)) {
+        //        0.1秒ごとに変数チェック
+        //        response処理待ち
+    }
+    return tags
+}
+
+func refresh_articles() -> [[String: String?]] {
+    var keep:Bool = false
+    var ref_articles:[[String: String?]] = []
+    //    以下、記事取得
+    Alamofire.request(.GET, "https://qiita.com/api/v2/items")
+        .responseJSON { response in
+            guard let object = response.result.value else {
+                return
+            }
+            let json_data = JSON(object)
+            json_data.forEach{ (_, json_data) in
+                let article: [String:String?] = [
+                    "title":json_data["title"].string,
+                    "userID":json_data["user"]["id"].string,
+                    "url":json_data["url"].string
+                ]
+                ref_articles.append(article)
+            }
+            keep = true
+    }
+    let runloop = NSRunLoop()
+    while keep && runloop.runMode(NSDefaultRunLoopMode, beforeDate: NSDate(timeIntervalSinceNow: 0.1)) {
+        //        0.1秒ごとに変数チェック
+        //        response処理待ち
+    }
+    return ref_articles
 }
--- QiitaTestApp/QiitaTestApp/ViewController.swift
+++ QiitaTestApp/QiitaTestApp/ViewController.swift
@@ -13,18 +13,36 @@ import SwiftyJSON
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    
+//  取得した記事の一覧を表示するテーブル
     @IBOutlet weak var qiitaView: UITableView!
-    
+//  表示する記事を保持
     var articles: [[String: String?]] = []
-
+    var ref_articles: [[String: String?]] = []
+    
+//  取得したタグの一覧を保持
+    var tags:[String] = []
+    
+    var refreshControl:UIRefreshControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         qiitaView.delegate = self
         qiitaView.dataSource = self
+        
+//      最新の記事データを取得
         articles = getArticles()
+//      取得したデータを表示するために再描画
         self.qiitaView.reloadData()
+        
+//      タグ一覧を取得
+        tags = getTags()
+        print(tags)
+
+        refreshControl = UIRefreshControl()
+        self.refreshControl.addTarget(self, action:#selector(ViewController.refresh), forControlEvents:.ValueChanged)
+        self.refreshControl.attributedTitle = NSAttributedString(string: "Refresh")
+        self.qiitaView.addSubview(refreshControl)
 
     }
     
@@ -33,16 +51,44 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // Dispose of any resources that can be recreated.
     }
     
+//  ”下に引っ張って更新”の中身
+    internal func refresh() {
+        if self.ref_articles.count != 0 {
+            self.ref_articles.removeAll()
+        }
+        self.ref_articles = refresh_articles()
+        self.refreshControl.endRefreshing()
+        if ref_articles.count != 0 {
+            var cnt:Int = 0
+            for article in self.ref_articles {
+                self.articles.insert(article, atIndex: cnt)
+                cnt += 1
+            }
+            self.qiitaView.reloadData()
+        }
+    }
+    
+    
+//  表示するセルの数
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.articles.count
     }
     
+//  セルに表示する内容
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell:UITableViewCell = qiitaView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
         let article = articles[indexPath.row]
         cell.textLabel?.text = article["title"]!
         cell.detailTextLabel?.text = article["userID"]!
         return cell
+    }
+    
+//  セルをタップした時の処理
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+//      セルの記事情報を取得
+        let article = articles[indexPath.row]
+//      タップしたらsafariでURL先を表示
+        UIApplication.sharedApplication().openURL(NSURL(string:article["url"]!!)!)
     }
 }
 
